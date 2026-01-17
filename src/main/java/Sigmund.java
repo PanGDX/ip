535fb5b30e79a8880b971262113d69e4dddbbfce
PanGDX <prtanprasertkul@gmail.com>
2026-01-17 20:39:34 +0800

    Complete Level-0
    Remove Duke.java
    
    Add Sigmund.java
    
    .gitignore: Add *.class to ignore Java class files

---
+++ src/main/java/Sigmund.java
@@ -0,0 +1,13 @@
+public class Sigmund {
+    public static void main(String[] args) {
+        String logo = """
+____________________________________________________________
+Hello! I'm Sigmund
+What can I do for you?
+I love Kopi C Kosong!
+____________________________________________________________
+Bye! Have a nice day!
+____________________________________________________________""".strip();
+        System.out.println(logo);
+    }
+}
