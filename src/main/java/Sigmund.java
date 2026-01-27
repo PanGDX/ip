89771f72f8d0797d6bdca16f36bdc61e58f56678
PanGDX <prtanprasertkul@gmail.com>
2026-01-27 21:41:00 +0800

    feat: echos commands entered by the user, and exits when the user types the command bye

--- src/main/java/Sigmund.java
+++ src/main/java/Sigmund.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Sigmund {
     public static void main(String[] args) {
         String logo = """
@@ -5,9 +7,24 @@ ____________________________________________________________
 Hello! I'm Sigmund
 What can I do for you?
 I love Kopi C Kosong!
-____________________________________________________________
-Bye! Have a nice day!
 ____________________________________________________________""".strip();
         System.out.println(logo);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            String line = scanner.nextLine(); 
+            System.out.println("____________________________________________________________");
+            
+            if(line.equals("bye")){
+                System.out.println("BYE BYE! See you again!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println(line);
+
+            System.out.println("____________________________________________________________");
+        }
+        scanner.close(); 
     }
 }
