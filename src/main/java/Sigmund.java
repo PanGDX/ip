1fc7a3dcc8f890d7a0b60250aa7f27785828c45f
PanGDX <prtanprasertkul@gmail.com>
2026-01-27 21:51:54 +0800

    feat: Add the ability to store whatever text entered by the user and display them back to the user when requested

--- src/main/java/Sigmund.java
+++ src/main/java/Sigmund.java
@@ -1,30 +1,43 @@
 import java.util.Scanner;
 
 public class Sigmund {
+    public static String[] storage = new String[100];
+    public static int itemCount = 0;
+
     public static void main(String[] args) {
+
         String logo = """
-____________________________________________________________
-Hello! I'm Sigmund
-What can I do for you?
-I love Kopi C Kosong!
-____________________________________________________________""".strip();
+                ____________________________________________________________
+                Hello! I'm Sigmund
+                What can I do for you?
+                I love Kopi C Kosong!
+                ____________________________________________________________""".strip();
         System.out.println(logo);
 
         Scanner scanner = new Scanner(System.in);
 
-        while(true){
-            String line = scanner.nextLine(); 
+        while (true) {
+            String line = scanner.nextLine();
             System.out.println("____________________________________________________________");
-            
-            if(line.equals("bye")){
+
+            if (line.toLowerCase().equals("bye") || line.toLowerCase().equals("exit")) {
                 System.out.println("BYE BYE! See you again!");
                 System.out.println("____________________________________________________________");
                 break;
             }
-            System.out.println(line);
-
+            else if (line.equals("list")) {
+                for (int i = 0; i < itemCount; i++) {
+                    System.out.println((i + 1) + ". " + storage[i]);
+                }
+            } 
+            else {
+                // Save the input to our class-wide variable
+                storage[itemCount] = line;
+                itemCount++;
+                System.out.println("added: " + line);
+            }
             System.out.println("____________________________________________________________");
         }
-        scanner.close(); 
+        scanner.close();
     }
 }
