3b3519d559f7993eead8744c76d34f5f11637e3d
PanGDX <prtanprasertkul@gmail.com>
2026-02-17 12:34:28 +0800

    test: modify text-ui-test to handle compiling java into bin, compiling .jar properly and deleting relevant test folders like data

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -1,38 +1,64 @@
 #!/usr/bin/env bash
 
-# create bin directory if it doesn't exist
-if [ ! -d "../bin" ]
-then
-    mkdir ../bin
+
+
+# Define variables
+JAR_FILE="ip.jar"
+MAIN_CLASS="Sigmund.Sigmund" # Package.Class
+
+# 1. CLEANUP
+# Remove the bin directory and previous output files to ensure a fresh build
+if [ -d "bin" ]; then
+    rm -rf bin
 fi
 
-# delete output from previous run
-if [ -e "./ACTUAL.TXT" ]
-then
-    rm ACTUAL.TXT
-fi
+rm -f "$JAR_FILE" ACTUAL.TXT
 
-# compile the code into the bin folder
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
+# Create bin directory
+mkdir bin
+
+# 2. COMPILE
+# -d bin: Output compiled classes to the bin folder
+# -sourcepath src/main/java: Tells javac where to look for other packages (like 'data') if Sigmund imports them
+# src/main/java/Sigmund/*.java: Only target the files in the Sigmund folder for compilation (dependencies will be pulled in automatically)
+if ! javac -d bin -sourcepath src/main/java src/main/java/Sigmund/*.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
-# run the program and redirect output
-java -classpath ../bin Sigmund < input.txt > ACTUAL.TXT
+# 3. CREATE JAR
+# c: create, f: file, e: entry-point
+# -C bin .: Go into the 'bin' folder and include everything ('.') inside it
+if ! jar cfe "$JAR_FILE" "$MAIN_CLASS" -C bin .
+then
+    echo "********** JAR CREATION FAILURE **********"
+    exit 1
+fi
 
+pwd
+cd ./text-ui-test
+pwd
 
-# This removes the \u001B[...m sequences so diff doesn't see them
+if [ -d "data" ]; then
+    rm -rf data
+fi
+
+# 4. RUN TEST
+# Run the program using the generated jar
+java -jar "../$JAR_FILE" < input.txt > ACTUAL.TXT
+
+# --- VERIFICATION (Standard diff logic) ---
+
+# Remove ANSI colors
 sed -i 's/\x1b\[[0-9;]*m//g' ACTUAL.TXT
 
-# compare the output
+# Compare output
 cp EXPECTED.TXT EXPECTED-UNIX.TXT
-dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
+dos2unix -n ACTUAL.TXT ACTUAL-UNIX.TXT
+dos2unix -n EXPECTED-UNIX.TXT EXPECTED-UNIX.TXT
 
-# compare the output to the expected output
-diff ACTUAL.TXT EXPECTED-UNIX.TXT
-if [ $? -eq 0 ]
+if diff ACTUAL-UNIX.TXT EXPECTED-UNIX.TXT
 then
     echo "Test result: PASSED"
     exit 0
