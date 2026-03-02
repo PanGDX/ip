4136e4b5a6ff20097b290d3a93a2c2087e9a0645
PanGDX <prtanprasertkul@gmail.com>
2026-03-02 17:13:44 +0800

    style: change color to increase intensity

--- src/main/java/Sigmund/TextColor.java
+++ src/main/java/Sigmund/TextColor.java
@@ -4,9 +4,9 @@ package Sigmund;
  * Enum containing ANSI escape code for printing colored text
  */
 public enum TextColor {
-    RED("\u001B[31m"),
-    GREEN("\u001B[32m"),
-    BLUE("\u001B[34m");
+    RED("\u001B[91m"), // High Intensity Red
+    GREEN("\u001B[92m"), // High Intensity Green
+    BLUE("\u001B[94m"); // High Intensity Blue
 
     private final String ansiCode;
 
